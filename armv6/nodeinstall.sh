09de6a986f2ab086972e98cbb1b9cb338b5d0432
vzakharchenko <vzakharchenko@modeln.com>
2021-03-14 11:09:36 +0200

    added support arm/v6

--- armv6/nodeinstall.sh
+++ armv6/nodeinstall.sh
@@ -1,2 +1,2 @@
 export NODE_VER=15.6.0
-curl -sSL https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v${NODE_VER}.sh | bash
+wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v${NODE_VER}.sh | bash
